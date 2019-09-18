071e119a30b918cf81d7aa620ee80055095ab365
afzal <afzal@LAPTOP-6FPLRSTU.localdomain>
2019-09-18 10:56:35 +0100

    updated builder example

--- DungeonsAndDesignPatterns/Builder/Builder.cs
+++ DungeonsAndDesignPatterns/Builder/Builder.cs
@@ -29,17 +29,20 @@ namespace Builder
         public override void BuildLand()
         {
             
-            Console.WriteLine("Land");
+            forrest.Length = 8;   
+            Console.WriteLine("Forrest land length: {0}" , forrest.Length);
         }
 
         public override void BuildTrees()
         {
-            Console.WriteLine("Built");
+            forrest.NumberOfTrees = 10; 
+            Console.WriteLine("Trees built: {0}" , forrest.NumberOfTrees);
         }
 
         public override void BuildSpeciality()
         {
-            Console.WriteLine("Sp");
+            forrest.SpecialFeature = "Dragons populated";
+            Console.WriteLine("Special feature of this forrest: {0}" , forrest.SpecialFeature);
 
         }
     }
@@ -96,20 +99,29 @@ namespace Builder
         {
             Wizard wizard = new Wizard();
             DinosaursHighForrestBuilder dino = new DinosaursHighForrestBuilder();
-
+            DragonHighForrestBuilder dragon = new DragonHighForrestBuilder();
+            
             // Use make forest and dino to build the forrest with dinosaurs.
+            Console.WriteLine("Dinosaurs forrest builder output");
             wizard.MakeForrest(dino);
 
-            HighForrest forrest = wizard.GetForrest(dino);
+            // Use make forest and dragons to build the forrest with dragons.
+            Console.WriteLine("Dragon forrest builder");
+            wizard.MakeForrest(dragon);
+
+            // Retrieve the composite/constructed object
+            HighForrest forrestWithDinosaurs = wizard.GetForrest(dino);
+            HighForrest forrestWithDragon = dragon.GetForrest();
             
-            Console.WriteLine("To test if right type of forrest. Forrest Length = {0}", forrest.SpecialFeature);
-            
-            //Output
-            
-            // Forrest land length: 5
-            // Trees built: 7
-            // Special feature of this forrest: Dinosaurs populated
-            // To test if right type of forrest. Forrest Length = Dinosaurs populated
+            //output
+            //Dinosaurs forrest builder output
+            //Forrest land length: 5
+            //Trees built: 7
+            //Special feature of this forrest: Dinosaurs populated
+            //Dragon forrest builder
+            //Forrest land length: 8
+            //Trees built: 10
+            //Special feature of this forrest: Dragons populated
 
         }
     }
